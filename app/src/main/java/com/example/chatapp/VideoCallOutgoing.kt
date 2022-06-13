ca6d9088917ecceffc1eea8f86be4d701ed79d28
Hieu <thanhhieu995@gmail.com>
2022-06-14 00:41:30 +0700

    Transfer the user friend name and avatar name to the out going call

--- app/src/main/java/com/example/chatapp/VideoCallOutgoing.kt
+++ app/src/main/java/com/example/chatapp/VideoCallOutgoing.kt
@@ -4,8 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.chatapp.chat.ChatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
 
 class VideoCallOutgoing : AppCompatActivity() {
 
@@ -16,16 +21,29 @@ class VideoCallOutgoing : AppCompatActivity() {
     lateinit var userLogin: User
     lateinit var userFriend: User
 
+    lateinit var txtNameOutgoing: TextView
+    lateinit var imgAvatarOutgoing: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_call_outgoing)
 
         btnDecline = findViewById(R.id.btn_decline_outgoing)
+        txtNameOutgoing = findViewById(R.id.txtName_outgoing)
+        imgAvatarOutgoing = findViewById(R.id.img_avatar_outgoing)
+
         loginUid = intent.getSerializableExtra("uidLogin") as String
         friendUid = intent.getSerializableExtra("uidFriend") as String
         hasMore = intent.getBooleanExtra("hasMore", false)
         userLogin = intent.getSerializableExtra("userLogin") as User
         userFriend = intent.getSerializableExtra("userFriend") as User
+
+        txtNameOutgoing.text = userFriend.name
+
+       FirebaseStorage.getInstance().reference.child("images").child(friendUid)
+           .downloadUrl.addOnSuccessListener {
+               Picasso.get().load(it).into(imgAvatarOutgoing)
+           }
 
         btnDecline.setOnClickListener {
             val intent = Intent(this@VideoCallOutgoing, ChatActivity::class.java)
