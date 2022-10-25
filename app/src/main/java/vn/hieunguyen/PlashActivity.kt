7d8bde7a810e7dcbd74f523e5b93dca84dc11b78
Hieu <thanhhieu995@gmail.com>
2022-10-25 11:06:53 +0700

    Create plashAcitivity.

---
+++ app/src/main/java/vn/hieunguyen/PlashActivity.kt
@@ -0,0 +1,12 @@
+package vn.hieunguyen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import vn.hieunguyen.chatapp.R
+
+class PlashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plash)
+    }
+}
