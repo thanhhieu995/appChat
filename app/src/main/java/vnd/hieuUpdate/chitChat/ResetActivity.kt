850883a19219531531d3fd8338e6bf750f03a09d
Hieu <thanhhieu995@gmail.com>
2022-10-27 14:41:40 +0700

    Create resetActivity

---
+++ app/src/main/java/vnd/hieuUpdate/chitChat/ResetActivity.kt
@@ -0,0 +1,11 @@
+package vnd.hieuUpdate.chitChat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset)
+    }
+}
